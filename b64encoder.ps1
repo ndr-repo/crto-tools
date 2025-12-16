cc31268c02d6fd6b32f1adb89c39782068363054
weekndr_sec <172009322+ndr-repo@users.noreply.github.com>
2025-12-15 22:28:10 -0500

    Update output format for encoded PowerShell command

--- b64encoder.ps1
+++ b64encoder.ps1
@@ -1,8 +1,8 @@
 # $command = 'cmd /C cmdkey /list ' ; $bytes = [System.Text.Encoding]::Unicode.GetBytes($command) ; $encodedCommand = [Convert]::ToBase64String($bytes) ; Write-Output $command ; Write-Output $encodedCommand
 # cmd /C cmdkey /list
 # YwBtAGQAIAAvAEMAIABjAG0AZABrAGUAeQAgAC8AbABpAHMAdAAgAA==
-# 
+# powershell -nop -EncodedCommand YwBtAGQAIAAvAEMAIABjAG0AZABrAGUAeQAgAC8AbABpAHMAdAAgAA==
 $command = $args[0] 
 $bytes = [System.Text.Encoding]::Unicode.GetBytes($command) 
 $encodedCommand = [Convert]::ToBase64String($bytes)
-Write-Output $command ; Write-Output $encodedCommand ; Write-Output powershell -nop -EncodedCommand $encodedCommand
+Write-Output $command ; Write-Output $encodedCommand ; Write-Output "powershell -nop -EncodedCommand $encodedCommand"
